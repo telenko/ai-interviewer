89b00056725e86a05cba8cf474ceb8524393dc22
Andrii Telenko <mangolik931@gmail.com>
2025-07-18 17:55:26 +0200

    UI validation for create vacancy modal

--- frontend/src/modules/Vacancies/AddVacancyModal.tsx
+++ frontend/src/modules/Vacancies/AddVacancyModal.tsx
@@ -11,8 +11,13 @@ import { Input } from '@/components/ui/input';
 import InterviewerApi from '@/services/InterviewerApi';
 import { useAddVacancyMutation } from '@/services/vacancyApi';
 import { Loader2Icon, PlusIcon, ClipboardPaste, ArrowLeft } from 'lucide-react';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import { z } from 'zod';
+
+const ErrorLayout = (props: { msg: string }) => (
+  <p className="mt-1 text-sm text-red-600 absolute bottom-[-18px] left-2">{props.msg}</p>
+);
 
 export default function AddVacancyModal({ open, onClose }: { open: boolean; onClose: () => void }) {
   const [url, setUrl] = useState('');
@@ -22,16 +27,76 @@ export default function AddVacancyModal({ open, onClose }: { open: boolean; onCl
   const [loadingFromUrl, setLoadingFromUrl] = useState(false);
   const [langCode, setLangCode] = useState<string>();
   const [addVacancy, { isLoading: vacancyCreationLoading }] = useAddVacancyMutation();
+  const [touched, setTouched] = useState<{ title?: boolean; url?: boolean; skills?: boolean }>({});
   const { t } = useTranslation();
 
-  const isValid = !!title && (skills.length > 0 || !!inputSkill);
+  const closeModal = useCallback(() => {
+    setUrl('');
+    setTitle('');
+    setSkills([]);
+    setLangCode('');
+    setTouched({});
+    onClose();
+  }, [onClose]);
+
+  const validationSchema = useMemo(
+    () =>
+      z.object({
+        title: z
+          .string()
+          .max(30, t('createVacancy.errors.title', { min: 3, max: 30 }))
+          .min(3, t('createVacancy.errors.title', { min: 3, max: 30 })),
+        skills: z
+          .array(
+            z
+              .string()
+              .max(30, t('createVacancy.errors.skill', { min: 2, max: 30 }))
+              .min(2, t('createVacancy.errors.skill', { min: 2, max: 30 })),
+          )
+          .max(30, t('createVacancy.errors.skillsMax', { max: 30 }))
+          .min(1, t('createVacancy.errors.skillsMin', { min: 30 })),
+        url: z.union(
+          [z.url(t('createVacancy.errors.url')), z.literal('')],
+          t('createVacancy.errors.url'),
+        ),
+      }),
+    [],
+  );
+
+  const computedSkills = useMemo(
+    () => (skills.length > 0 ? skills : [inputSkill]),
+    [skills, inputSkill],
+  );
+  const validation = useMemo(
+    () =>
+      validationSchema.safeParse({
+        title,
+        skills: computedSkills,
+        url,
+      }),
+    [url, title, computedSkills],
+  );
+  const isValid = !validation.error;
+
+  const titleIssue = useMemo(
+    () => validation?.error?.issues?.find((iss) => iss.path.includes('title')),
+    [validation],
+  );
+  const urlIssue = useMemo(
+    () => validation?.error?.issues?.find((iss) => iss.path.includes('url')),
+    [validation],
+  );
+  const skillsIssue = useMemo(
+    () => validation?.error?.issues?.find((iss) => iss.path.includes('skills')),
+    [validation],
+  );
 
   useEffect(() => {
     if (!open) return;
 
     const handlePopState = (e: PopStateEvent) => {
       e.preventDefault();
-      onClose();
+      closeModal();
       window.history.pushState(null, '', window.location.href); // блокуємо навігацію назад
     };
 
@@ -41,7 +106,7 @@ export default function AddVacancyModal({ open, onClose }: { open: boolean; onCl
     return () => {
       window.removeEventListener('popstate', handlePopState);
     };
-  }, [open, onClose]);
+  }, [open, closeModal]);
 
   const onAutofill = async () => {
     setLoadingFromUrl(true);
@@ -58,14 +123,20 @@ export default function AddVacancyModal({ open, onClose }: { open: boolean; onCl
       const vacancyCut = response.data.vacancy_cut;
       setTitle(vacancyCut.title);
       setSkills(vacancyCut.skills);
+      setTouched((prev) => ({
+        ...prev,
+        title: true,
+        skills: true,
+      }));
     } finally {
       setLoadingFromUrl(false);
     }
   };
 
   const addSkill = () => {
-    if (inputSkill.trim() && !skills.includes(inputSkill.trim())) {
-      setSkills((prev) => [...prev, inputSkill.trim()]);
+    const newSkill = inputSkill.trim().slice(0, 30);
+    if (newSkill && !skills.includes(newSkill)) {
+      setSkills((prev) => [...prev, newSkill]);
     }
     setInputSkill('');
   };
@@ -75,13 +146,13 @@ export default function AddVacancyModal({ open, onClose }: { open: boolean; onCl
   };
 
   return (
-    <Dialog open={open} onOpenChange={(isOpen) => !isOpen && onClose()}>
+    <Dialog open={open} onOpenChange={(isOpen) => !isOpen && closeModal()}>
       <DialogContent className="w-full h-screen sm:max-h-screen overflow-y-auto max-w-none rounded-none sm:max-w-lg sm:h-auto sm:rounded-xl">
         <DialogHeader className="flex flex-row items-center justify-between px-1 sm:px-0">
           <Button
             variant="outline"
             size="icon"
-            onClick={onClose}
+            onClick={closeModal}
             className="absolute left-5 sm:hidden"
           >
             <ArrowLeft className="w-5 h-5" />
@@ -91,58 +162,94 @@ export default function AddVacancyModal({ open, onClose }: { open: boolean; onCl
           </DialogTitle>
         </DialogHeader>
 
-        <div className="flex flex-col space-y-5 pb-4 pt-2 mt-0">
+        <div className="flex flex-col space-y-6 pb-4 pt-2 mt-0">
           {/* URL + кнопка */}
-          <div className="flex gap-2 relative">
-            <Input
-              className="py-6 blink-green-border placeholder:text-base pr-15 sm:pr-4"
-              placeholder={t('gentle_url_placeh')}
-              value={url}
-              onChange={(e) => setUrl(e.target.value)}
-            />
-            {!url ? (
-              <>
+          <div className="relative">
+            <div className="flex gap-2 relative">
+              <Input
+                className="py-6 blink-green-border placeholder:text-base pr-15 sm:pr-4"
+                placeholder={t('gentle_url_placeh')}
+                value={url}
+                onBlur={() => {
+                  setTouched((prev) => ({
+                    ...prev,
+                    url: true,
+                  }));
+                }}
+                onChange={(e) => {
+                  setUrl(e.target.value);
+                }}
+              />
+              {!url ? (
+                <>
+                  <Button
+                    onClick={async () => {
+                      try {
+                        const clipboardText = await navigator.clipboard.readText();
+                        setUrl(clipboardText);
+                      } catch {}
+                    }}
+                    className="absolute right-2 top-1/2 -translate-y-1/2 w-10 h-10 flex items-center justify-center gap-2 bg-blue-600 hover:bg-blue-700 active:bg-blue-800 transition text-white px-4 py-2 rounded-md md:hidden"
+                    aria-label="Вставити з буфера"
+                    type="button"
+                  >
+                    <ClipboardPaste className="w-5 h-5" />
+                  </Button>
+                </>
+              ) : null}
+              {url && (
                 <Button
-                  onClick={async () => {
-                    try {
-                      const clipboardText = await navigator.clipboard.readText();
-                      setUrl(clipboardText);
-                    } catch {}
-                  }}
-                  className="absolute right-2 top-1/2 -translate-y-1/2 w-10 h-10 flex items-center justify-center gap-2 bg-blue-600 hover:bg-blue-700 active:bg-blue-800 transition text-white px-4 py-2 rounded-md md:hidden"
-                  aria-label="Вставити з буфера"
-                  type="button"
+                  variant="secondary"
+                  onClick={onAutofill}
+                  className="h-12 text-base"
+                  disabled={loadingFromUrl}
                 >
-                  <ClipboardPaste className="w-5 h-5" />
+                  {loadingFromUrl ? (
+                    <Loader2Icon className="w-4 h-4 animate-spin" />
+                  ) : (
+                    t('generate')
+                  )}
                 </Button>
-              </>
-            ) : null}
-            {url && (
-              <Button
-                variant="secondary"
-                onClick={onAutofill}
-                className="h-12 text-base"
-                disabled={loadingFromUrl}
-              >
-                {loadingFromUrl ? <Loader2Icon className="w-4 h-4 animate-spin" /> : t('generate')}
-              </Button>
-            )}
+              )}
+            </div>
+            {touched.url && urlIssue?.message ? <ErrorLayout msg={urlIssue?.message} /> : null}
           </div>
 
           {/* Title */}
-          <Input
-            placeholder={t('vacancy_name')}
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-          />
+          <div className="relative">
+            <Input
+              placeholder={t('vacancy_name')}
+              value={title}
+              onBlur={() => {
+                setTouched((prev) => ({
+                  ...prev,
+                  title: true,
+                }));
+              }}
+              onChange={(e) => {
+                setTitle(e.target.value);
+              }}
+            />
+            {touched.title && titleIssue?.message ? (
+              <ErrorLayout msg={titleIssue?.message} />
+            ) : null}
+          </div>
 
           {/* Multi-skill input */}
-          <div>
+          <div className="relative">
             <div className="flex gap-2 mb-2">
               <Input
                 placeholder={t('enter_skills')}
                 value={inputSkill}
-                onChange={(e) => setInputSkill(e.target.value)}
+                onBlur={() => {
+                  setTouched((prev) => ({
+                    ...prev,
+                    skills: true,
+                  }));
+                }}
+                onChange={(e) => {
+                  setInputSkill(e.target.value);
+                }}
                 onKeyDown={(e) => {
                   if (e.key === 'Enter') {
                     e.preventDefault();
@@ -170,14 +277,17 @@ export default function AddVacancyModal({ open, onClose }: { open: boolean; onCl
                 </span>
               ))}
             </div>
+            {touched.skills && skillsIssue?.message ? (
+              <ErrorLayout msg={skillsIssue?.message} />
+            ) : null}
           </div>
 
           {/* Lang_code */}
-          <LanguageSelect value={langCode} onChange={setLangCode} className="mt-[-8px]" />
+          <LanguageSelect value={langCode} onChange={setLangCode} className="mt-[-2px]" />
         </div>
         {/* on some mobile devices bottom of footer not visible, so adding mb-4 */}
         <DialogFooter className="mt-4 mb-4 sm:mb-0">
-          <Button onClick={onClose} variant="outline">
+          <Button onClick={closeModal} variant="outline">
             {t('cancel')}
           </Button>
           <Button
@@ -187,7 +297,7 @@ export default function AddVacancyModal({ open, onClose }: { open: boolean; onCl
                 title,
                 skills: skills.length > 0 ? skills : [inputSkill],
                 url,
-              }).then((e) => (!e.error ? onClose() : null));
+              }).then((e) => (!e.error ? closeModal() : null));
             }}
             disabled={!isValid}
           >
